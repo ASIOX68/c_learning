0a9039d040210a0225d8ad2966170df3e9be45bc
ASIOX68 <asiox.68@gmail.com>
2026-03-12 23:48:04 +0100

    add MD_array in multidimensionnal_array

---
+++ multidimensionnal_array/MD_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    int matrix[][4] = {{14, 10, 6, 4}, {3, 7, 18, 11}, {13, 9, 5, 17}, {19, 12, 2, 1}};
+    int sum = 0;
+
+    int num = matrix[3][1];
+    printf("%d\n", num);
+
+    int rowDimension = sizeof(matrix)/sizeof(matrix[0]);
+    int columnDimension = sizeof(matrix[0])/sizeof(int);
+
+    for(int i = 0; i < rowDimension; i++){
+        for(int j = 0; j < columnDimension; j++){
+            sum += matrix[i][j];
+        }
+    }
+
+    printf("%i\n", sum);
+
+    return 0;
+}
